cc12c8fa49935ec2aa55a045d875189883a4c4b2
mohamed-amer404 <mohamed.elsoqier@gmail.com>
2026-02-12 00:22:10 +0200

    upgrade script create s3 bucket

--- scripts/create-s3-backends.sh
+++ scripts/create-s3-backends.sh
@@ -1,19 +1,46 @@
- ######### Make S3 Bucket to networking module & enable Versioning ############
-echo "Starting Create S3 Networking Buckets... "
-aws s3 mb s3://varrow-academy-cloud-networking-terraform-backend-us-east-1 --region us-east-1
-aws s3api put-bucket-versioning --bucket varrow-academy-cloud-networking-terraform-backend-us-east-1 --versioning-configuration Status=Enabled
  
- 
- 
- ######### Make S3 Bucket to Compute module & enable Versioning ############
-echo "Starting Create S3 Compute Buckets... "
-aws s3 mb s3://varrow-academy-cloud-compute-terraform-backend-us-east-1 --region us-east-1
-aws s3api put-bucket-versioning --bucket varrow-academy-cloud-compute-terraform-backend-us-east-1 --versioning-configuration Status=Enabled
+ #!/bin/bash
+ ######### Make S3 Bucket to Backend & enable Versioning ############
 
-  ####  Test Buckets after excuted #####
-echo "Starting Test Buckets... "
+ echo " ------- Starting Create S3 Buckets... "
+ read -p "Enter the (( number )) of buckets : " num
+ count="$num"
+ name_backets=()
+while [ 1 -le $num ]
+do
+    read -p "Enter the bucket name : " bucket_name 
+    read -p "Enter the region : " region_name 
+
+    aws s3 mb s3://$bucket_name-terraform-backend-$region_name --region $region_name
+
+      read -p "Do you enable Versioning in this Bucket (yes / no): " yes_no
+
+           if [ $yes_no = "yes" ]; then
+
+              aws s3api put-bucket-versioning --bucket $bucket_name-terraform-backend-$region_name --versioning-configuration Status=Enabled
+
+           elif [  $yes_no = "no" ]; then
+              echo " ---- No versioning in this bucket ---- "
+           else
+              aws s3 rb s3://$bucket_name-terraform-backend-$region_name
+              break
+           fi
+
+     name_backets+=("$bucket_name-terraform-backend-$region_name")      
+
+    ((num--)) 
+done
+
+  ################  Test Buckets after excuted #################
+
+echo "Starting List & Test Buckets... "
  aws s3 ls
-echo "Starting Test Versioning for Compute Bucket... "
- aws s3api get-bucket-versioning --bucket varrow-academy-cloud-compute-terraform-backend-us-east-1
-echo "Starting Test Versioning for Network Bucket... "
- aws s3api get-bucket-versioning --bucket varrow-academy-cloud-networking-terraform-backend-us-east-1
+       while [ $num -le $((count - 1)) ]
+       do
+          
+           echo "Starting Test Versioning for ${name_backets[$num]}... "
+           aws s3api get-bucket-versioning --bucket ${name_backets[$num]}
+
+          ((num++))
+
+       done
